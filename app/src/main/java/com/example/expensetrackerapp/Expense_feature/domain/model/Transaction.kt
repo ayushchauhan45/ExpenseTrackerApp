f3941a819b18aaf347c50277add4f4c3a28c7c46
ayushchauhan45 <chauhanasc2004@gmail.com>
2025-03-17 19:48:41 +0530

    Change the db entity to have one to many relations

--- app/src/main/java/com/example/expensetrackerapp/Expense_feature/domain/model/Transaction.kt
+++ app/src/main/java/com/example/expensetrackerapp/Expense_feature/domain/model/Transaction.kt
@@ -1,13 +1,26 @@
 package com.example.expensetrackerapp.Expense_feature.domain.model
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.util.Date
 
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["id"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Transaction(
     @PrimaryKey(autoGenerate = true)
-    val id: Int?= null,
+    val transactionId: Int?= null,
     val amount: Double,
     val category: String,
+    val type: String,
     val date: Date,
     val paymentType: String
 )
--- app/src/main/java/com/example/expensetrackerapp/Expense_feature/domain/model/User.kt
+++ app/src/main/java/com/example/expensetrackerapp/Expense_feature/domain/model/User.kt
@@ -1,13 +1,14 @@
 package com.example.expensetrackerapp.Expense_feature.domain.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.Date
 
+@Entity
 data class User(
     @PrimaryKey(autoGenerate = true)
     val id: Int?= null,
     val name: String,
-    val income: Double,
-    val spent: Double,
-    val saving: Double
+    val balance: Double =0.0,
+    val spent: Double= 0.0,
 )
