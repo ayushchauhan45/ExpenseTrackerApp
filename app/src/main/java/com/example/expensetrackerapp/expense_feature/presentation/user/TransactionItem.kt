48bbd53ce8ec72a5ad47bf8949f1f7b466d39dfb
ayushchauhan45 <chauhanasc2004@gmail.com>
2025-03-22 23:42:50 +0530

    Adding custom scaffold on Add transaction Screen

--- app/src/main/java/com/example/expensetrackerapp/expense_feature/presentation/user/TransactionItem.kt
+++ app/src/main/java/com/example/expensetrackerapp/expense_feature/presentation/user/TransactionItem.kt
@@ -1,7 +1,5 @@
 package com.example.expensetrackerapp.expense_feature.presentation.user
 
-import android.content.res.Resources
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,15 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.expensetrackerapp.R
